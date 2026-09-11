2485081d732c62b1b499328d7f7d759e5ff5b842
Lev1-kwh <2705727884@qq.com>
2026-09-11 20:57:42 +0800

    feat: add paper create api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+from app.models import PaperCreate
 app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "PaperAgent is running"}
+@app.post("/papers")
+def create_paper(paper:PaperCreate):
+    response={}
+    response["message"]="paper created"
+    response["title"]=paper.title
+    response["author"]=paper.author
+    return response
