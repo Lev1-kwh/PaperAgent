63953e36ce40b29c1ced27f7cbe196d0db0362c4
Lev1-kwh <2705727884@qq.com>
2026-09-15 11:52:54 +0800

    feat: implement PDF upload

--- app/routers/paper.py
+++ app/routers/paper.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
 from app.models.paper import PaperResponse,PaperCreate
+from fastapi import APIRouter,UploadFile,File,HTTPException
+import os,uuid
 router = APIRouter(prefix="/papers")
 @router.post("",
              response_model=PaperResponse,
@@ -9,3 +10,16 @@ def create_paper(paper:PaperCreate):
                             title=paper.title,
                             author=paper.author)
    return response
+@router.post("/upload")
+async def  uploads_paper(file:UploadFile = File(...)):
+    content = await file.read()
+    filename = file.filename
+    extension = os.path.splitext(filename)[1].lower()
+    if extension!= ".pdf":
+        raise HTTPException(status_code=400,
+                            detail="只允许上传pdf文件")
+    unique_filename = str(uuid.uuid4()) + extension
+    with open(os.path.join("uploads",unique_filename),"wb") as f: f.write(content)
+    message = "保存成功"
+    return message
+
